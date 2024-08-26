fdaf0e185126c807440a9692a9978691ea91ee1c
dhrruvchotai <23010101051@darshan.ac.in>
2024-08-26 11:41:58 +0530

    Updated to Listen the speech.

--- Voice Assistant - Pyra/main.py
+++ Voice Assistant - Pyra/main.py
@@ -2,7 +2,7 @@ import speech_recognition as sr
 import webbrowser
 import pyttsx3
 
-recognizer = sr.Recognizer() #Initialized recognizer obj which recognizes if you speak something.
+r = sr.Recognizer() #Initialized recognizer obj which recognizes if you speak something.
 engine = pyttsx3.init()  #Initialized text to speech
 
 def speak(text):
@@ -10,4 +10,24 @@ def speak(text):
     engine.runAndWait()
 
 if( __name__ == "__main__"):
+
     speak("Initializing Pyra....")
+
+    while True:
+
+        #Listen for the wake word "Pyra"
+        # obtain audio from the microphone
+
+        with sr.Microphone() as source:
+            print("Listening....")
+            audio = r.listen(source)
+
+        # recognize speech using Google
+        print("Recognizing...")
+        try:
+            command = r.recognize_google(audio)
+            print(command)
+        except sr.UnknownValueError:
+            print("Google could not understand audio")
+        except sr.RequestError as e:
+            print("Google error; {0}".format(e))
