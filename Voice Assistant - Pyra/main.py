b517a3ee6ad9b9f8ec9b13c041a5b076e0511e06
dhrruvchotai <23010101051@darshan.ac.in>
2024-08-26 12:15:46 +0530

    Updated timeout and some exceptions in tts.

--- Voice Assistant - Pyra/main.py
+++ Voice Assistant - Pyra/main.py
@@ -2,7 +2,7 @@ import speech_recognition as sr
 import webbrowser
 import pyttsx3
 
-r = sr.Recognizer() #Initialized recognizer obj which recognizes if you speak something.
+r = sr.Recognizer() #Initialized recognizer obj which recognizes if you speak something. 
 engine = pyttsx3.init()  #Initialized text to speech
 
 def speak(text):
@@ -16,18 +16,17 @@ if( __name__ == "__main__"):
     while True:
 
         #Listen for the wake word "Pyra"
-        # obtain audio from the microphone
-
-        with sr.Microphone() as source:
-            print("Listening....")
-            audio = r.listen(source)
-
-        # recognize speech using Google
         print("Recognizing...")
+
         try:
-            command = r.recognize_google(audio)
+            # obtain audio from the microphone
+            with sr.Microphone() as source:
+                print("Listening....")
+                audio = r.listen(source,timeout=2,phrase_time_limit=5)
+
+            command = r.recognize_google(audio) # recognize speech using Google
             print(command)
-        except sr.UnknownValueError:
-            print("Google could not understand audio")
-        except sr.RequestError as e:
-            print("Google error; {0}".format(e))
+
+        except Exception as e:
+            print(f"Error...{format(e)}.")
+        
